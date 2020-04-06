bd0e0796da576e07b293394f5e50113411d7e5ea
Stephen Sinclair <radarsat1@gmail.com>
2020-04-06 10:54:11 +0200

    Plot multiple samples.

--- datasets/kicks.py
+++ datasets/kicks.py
@@ -5,6 +5,7 @@
 
 import matplotlib as mpl
 mpl.use('Agg')
+from matplotlib.cbook import flatten
 # mpl.rcParams['font.family'] = 'serif'
 # mpl.rcParams['font.serif'] = ['cmr10']
 # mpl.font_manager.findfont('cmmr')
@@ -52,32 +53,46 @@ class Dataset(object):
         self.dirname = dirname
         self.method = method
         self.variant = variant
-        self.fig, (self.ax1,self.ax2,self.ax3) = plt.subplots(1,3, num=1, figsize=(9,4))
+        self.fig, self.axs = plt.subplots(4,4, num=1, figsize=(9,9))
+        self.axs = list(flatten(self.axs))
         self.lims = None
         self.frame = 0
         self.x_input_viz = x_input
         self.eps_input_viz = eps_input
         self.z_input_viz = z_input
-        self.plt_sample = self.ax1.plot(x_input[0]*0)[0]
-        self.scat_z = self.ax2.scatter(z_input[:,0], z_input[:,1])
-        self.plt_orig = self.ax3.plot(x_input[0])[0]
-        self.plt_recon = self.ax3.plot(x_input[0]*0)[0]
-        self.ax1.set_title('decoded', fontname='cmr10')
-        self.ax3.set_title('reconstruction', fontname='cmr10')
+        self.plt_sample = []
+        self.plt_orig = []
+        self.plt_recon = []
+        for i in range(8):
+            self.plt_orig += self.axs[i*2+1].plot(x_input[i])
+            self.plt_recon += self.axs[i*2+1].plot(x_input[i]*0)
+            self.axs[i*2+1].set_yticks([])
+            if i % 2 >= 1:
+                self.axs[i*2].set_yticks([])
+            if i < 6:
+                self.axs[i*2].set_xticks([])
+                self.axs[i*2+1].set_xticks([])
+            if i < 3:
+                self.axs[i*2].set_title('decoded', fontname='cmr10')
+            if i < 2:
+                self.axs[i*2+1].set_title('reconstruction', fontname='cmr10')
+            if i > 0:
+                self.plt_sample += self.axs[i*2].plot(x_input[i-1]*0)
+        self.scat_z = self.axs[0].scatter(z_input[:,0], z_input[:,1])
         if self.params['latent_prior']=='normal':
-            self.ax2.plot(np.cos(np.linspace(0,2*np.pi,200))*1.96,
+            self.axs[0].plot(np.cos(np.linspace(0,2*np.pi,200))*1.96,
                           np.sin(np.linspace(0,2*np.pi,200))*1.96, 'k--', alpha=0.4)
-            self.ax2.set_xlim(-3,3)
-            self.ax2.set_ylim(-3,3)
+            self.axs[0].set_xlim(-3,3)
+            self.axs[0].set_ylim(-3,3)
         elif self.params['latent_prior']=='uniform':
-            self.ax2.plot([[0,1],[0,1],[0,0],[1,1]],
+            self.axs[0].plot([[0,1],[0,1],[0,0],[1,1]],
                           [[0,0],[1,1],[0,1],[0,1]], 'k--', alpha=0.4)
-            self.ax2.set_xlim(-0.5,1.5)
-            self.ax2.set_ylim(-0.5,1.5)
+            self.axs[0].set_xlim(-0.5,1.5)
+            self.axs[0].set_ylim(-0.5,1.5)
         if self.z_input_viz.shape[1]==2:
-            self.ax2.set_title('encoded', fontname='cmr10')
+            self.axs[0].set_title('encoded', fontname='cmr10')
         else:
-            self.ax2.set_title('encoded (first 2 dims)', fontname='cmr10')
+            self.axs[0].set_title('encoded (first 2 dims)', fontname='cmr10')
 
         if not os.path.exists(self.dirname):
             os.mkdir(self.dirname)
@@ -94,14 +109,16 @@ class Dataset(object):
             self.lims = yl_
         else:
             self.lims = self.lims*0.95+yl_*0.05
-        self.ax1.set_ylim(self.lims)
-        self.ax3.set_ylim(self.lims)
+        for i,ax in enumerate(self.axs):
+            if i!=0: ax.set_ylim(self.lims)
 
-        self.plt_sample.set_ydata(x_output[0])
+        for i,p in enumerate(self.plt_sample):
+            p.set_ydata(x_output[i])
         self.scat_z.set_offsets(z_output)
 
-        x_output = decoder([z_output[:1],self.eps_input_viz[:1]])
-        self.plt_recon.set_ydata(x_output[0])
+        x_output = decoder([z_output,self.eps_input_viz])
+        for i,p in enumerate(self.plt_recon):
+            p.set_ydata(x_output[i])
 
         self.fig.suptitle(
             f'{self.z_input_viz.shape[1]}D {self.method} {self.variant}: Epoch {epoch}',
