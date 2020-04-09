Fix gradient penalty, wrt *input* not weights.

utils/gp.py
@@ -3,6 +3,7 @@ __all__ = ['GradientPenalty']
 
 import tensorflow as tf
 import tensorflow.keras as tfk
+import numpy as np
 
 class GradientPenalty(tfk.layers.Layer):
     def __init__(self, params, critic):
@@ -15,11 +16,14 @@ class GradientPenalty(tfk.layers.Layer):
         elif self.variant=='1gp': targ = 1
         else: return tf.constant([0.0])
         r = tf.random.uniform([tf.shape(x1[0])[0],1])
+        x = [x1[0]*r + x2[0]*(1-r),
+             x1[1]*r + x2[1]*(1-r)]
         with tf.GradientTape() as tape:
-            x = [x1[0]*r + x2[0]*(1-r),
-                 x1[1]*r + x2[1]*(1-r)]
-            loss = self.critic(x)
+            tape.watch(x)
+            y_pred = self.critic(x)
         weights = [w for l in self.critic.layers for w in l.weights if len(l.weights)>0]
-        grads = tape.gradient(loss, [weights[0]])[0]
-        gradsl2 = tf.sqrt(tf.reduce_sum(grads**2, axis=1))
-        return tf.reshape(tf.square(targ - tf.reduce_mean(gradsl2)),(1,1))*self.weight
+        grads = tape.gradient(y_pred, x)
+        gradsl2 = [tf.reduce_sum(g**2, axis=np.arange(1,len(g.shape)))
+                   for g in grads if g is not None]
+        gradsl2 = tf.sqrt(tf.reduce_sum(gradsl2,axis=0) + 1e-10)
+        return tf.reshape(tf.reduce_mean(tf.square(targ - gradsl2)),(1,1))*self.weight