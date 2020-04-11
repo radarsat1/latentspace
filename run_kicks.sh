4adc276a87a3fe8721b5c6c2c20f4c78c7a3f81f
Stephen Sinclair <radarsat1@gmail.com>
2020-04-11 16:57:02 +0200

    Current settings for 16k audio BiGAN training.

--- run_kicks.sh
+++ run_kicks.sh
@@ -2,15 +2,18 @@
 
 python3 training.py \
         dataset.name=kicks \
-        dataset.data_dim=1024 \
+        dataset.data_dim=16384 \
         dataset.latent_dim=4 \
         dataset.latent_prior=uniform \
         model.eps_dim=1 \
         model.name=cnn1d \
-        model.type=veegan \
+        model.type=bigan \
         model.variant=0gp \
-        model.normalization.gen=batch \
-        training.learning_rate=1e-3 \
+        model.gp_weight=10 \
+        model.normalization.gen=none \
+        model.normalization.critic=none \
+        model.shape.filters=64 \
+        training.learning_rate=1e-4 \
         training.learning_rate_target=1e-4 \
-        training.critic_ratio=2 \
+        training.critic_ratio=3 \
         training.batch_size=32
